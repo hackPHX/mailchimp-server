d98bc149eb70f199ef3ccf0fb80daf680334b6ec
Blaine Bublitz <phate666x@gmail.com>
2012-10-04 19:00:12 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 8080);
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
---
+++ routes/index.js
@@ -0,0 +1,44 @@
+
+/*
+ * GET home page.
+ */
+
+var MailChimpAPI = require('mailchimp').MailChimpAPI;
+var config = require('../config');
+
+try {
+  var api = new MailChimpAPI(config.apiKey, { version : '1.3', secure : false });
+} catch (error) {
+  console.log(error.message);
+}
+
+// api.listMembers({
+//   id: listID
+// }, function (error, data) {
+//   if (error){
+//     console.log(error.message);
+//   } else {
+//     console.log(JSON.stringify(data)); // Do something with your data!
+//   }
+// });
+
+exports.index = function(req, res){
+  api.listSubscribe({
+    id: config.listID,
+    email_address: req.query.email,
+    merge_vars: {
+      FNAME: req.query.firstName,
+      LNAME: req.query.lastName
+    }
+  }, function (error, data) {
+    if (error){
+      res.end(error.message);
+    } else {
+      if(data === true){
+        res.end('Please check your email to confirm your subscription.');
+      } else {
+        res.end(JSON.stringify(data));
+      }
+    }
+  });
+};
